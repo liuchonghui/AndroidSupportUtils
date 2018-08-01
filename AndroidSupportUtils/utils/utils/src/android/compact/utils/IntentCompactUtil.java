7b5337c34e6d3167ff2210c13c265af6e111433c
liuchonghui <liuchonghui@xiaomi.com>
2018-08-01 14:12:10 +0800

    update to tools.android:utils:1.1.5
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

--- AndroidSupportUtils/utils/utils/src/android/compact/utils/IntentCompactUtil.java
+++ AndroidSupportUtils/utils/utils/src/android/compact/utils/IntentCompactUtil.java
@@ -3,6 +3,9 @@ package android.compact.utils;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
 
 public class IntentCompactUtil {
 
@@ -39,5 +42,42 @@ public class IntentCompactUtil {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static String getSegment(Intent intent, String key) {
+        String extraString = null;
+        if (intent != null) {
+            extraString = getSegment(intent.getExtras(), key);
+            if (TextUtils.isEmpty(extraString)) {
+                extraString = getSegment(intent.getData(), key);
+            }
+        }
+        return extraString;
+    }
+
+    public static String getSegment(Bundle extra, String key) {
+        String extraString = null;
+        if (extra != null) {
+            try {
+                extraString = extra.getString(key);
+            } catch (Throwable t) {
+                t.printStackTrace();
+                extraString = null;
+            }
+        }
+        return extraString;
+    }
+
+    public static String getSegment(Uri uri, String key) {
+        String extraString = null;
+        if (uri != null) {
+            try {
+                extraString = uri.getQueryParameter(key);
+            } catch (Throwable t) {
+                t.printStackTrace();
+                extraString = null;
+            }
+        }
+        return extraString;
     }
 }
